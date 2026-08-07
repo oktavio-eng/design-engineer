ff51e0623e4216cd0dcf1c31487f4b0726824762
oktavio <oktavio@gowstudio.pro>
2026-08-07 19:39:19 -0300

    Configure Storybook UI test runner

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,6 +1,7 @@
 /** @type {import('@storybook/html-vite').StorybookConfig} */
 const config = {
   stories: ["../stories/**/*.stories.js"],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-vitest"],
   framework: {
     name: "@storybook/html-vite",
     options: {},
